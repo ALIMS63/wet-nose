889916dc177ebc63fb748f2597252f79bd7f0547
Alim <shogen_077@mail.ru>
2020-09-18 15:13:22 +0300

    fixed authentication

--- front/src/redux/store.js
+++ front/src/redux/store.js
@@ -1,15 +1,23 @@
+import { useSelector } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { userReducer } from './reducers.js';
 import { composeWithDevTools } from 'redux-devtools-extension';
+
+// const auth = useSelector(state => state?.user);
+const preloadedState = window.localStorage.getItem('redux') || '{}';
 
 
 const store = createStore(
   combineReducers({
     user: userReducer,
   }),
-  undefined,
+  JSON.parse(preloadedState),
   composeWithDevTools()
 );
+
+store.subscribe(() => {
+  window.localStorage.setItem('redux', JSON.stringify(store.getState()));
+});
 
 
 export default store;
