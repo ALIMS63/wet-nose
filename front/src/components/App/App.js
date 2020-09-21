a051570d2b1e391cebd95380ed7256e5eb7ee5ae
Alim <shogen_077@mail.ru>
2020-09-21 09:33:48 +0300

    new animal cards

--- front/src/components/App/App.js
+++ front/src/components/App/App.js
@@ -14,6 +14,7 @@ import ModalRegister from '../Registration/ModalRegister';
 import Filter from '../Filter/Filter'
 import NewAnimal from '../NewAnimal/NewAnimal';
 import PetCart from '../PetCart';
+import AnimalCard from '../AnimalCard/AnimalCard';
 
 
 function App() {
@@ -25,8 +26,9 @@ function App() {
         <Switch>
           <Route path="/" exact>
             <Main />
-            <Filter/>
-            <PetCart/>
+            <Filter />
+            <PetCart />
+            {/* <AnimalCard /> */}
           </Route>
           <Route path="/login" exact>
             <Login />
