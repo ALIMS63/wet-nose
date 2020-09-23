3a37935450e53d89117ee97af4be156f21e29552
Рабочий <work@MacBook-Pro-Rabocij.local>
2020-09-23 18:06:09 +0300

    01

--- front/src/components/App/App.js
+++ front/src/components/App/App.js
@@ -71,9 +71,9 @@ function App() {
             <DeleteAnimal />
           </Route> */}
 
-          <PrivateRoute path="/secret">
+          <Route path="/secret">
             <NewAnimal />
-          </PrivateRoute>
+          </Route>
 
           {/* <PrivateRouter path="/user/:id" exact> */}
 
