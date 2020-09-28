073c39021befe4c45d132257ce8abcc78d0fa8bf
Рабочий <work@MacBook-Pro-Rabocij.local>
2020-09-28 11:39:17 +0300

    finish

--- front/src/components/Filter/CleanFilter.js
+++ front/src/components/Filter/CleanFilter.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { Button } from 'antd'
 import {useDispatch} from 'react-redux'
 import {cleanFilters} from '../../redux/actions'
+import store from '../../redux/store'
 
 
 function CleanFilter() {
   const dispatch = useDispatch()
   
   function clean (){
-    window.localStorage.removeItem('redux');
-    // dispatch(cleanFilters())
+    // window.localStorage.removeItem('redux', JSON.stringify(store.getState().animals.filters));
+    dispatch(cleanFilters())
     document.location.reload()
   }
 
